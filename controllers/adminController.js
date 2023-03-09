6be1841decf6bc8490b8462b47d2f9fce9bced84
Kyaw Zin Htet <itskyawzinhtet@gmail.com>
2023-03-09 15:43:50 +0630

    admin CRUD

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,21 +1,31 @@
 const Product = require("../models/Product");
 
+// Get All Products
 const getAllProducts = async (req, res) => {
   const products = await Product.find().sort("-createdAt");
   res.status(200).render("admin", { products });
 };
 
+// Get Create Product Form
 const getAddProduct = async (req, res) => {
   res.render("add-product");
 };
 
+// Create Product
 const createProduct = async (req, res) => {
   const product = await Product.create(req.body);
-  res.status(201).json({
+  res.redirect("/admin");
+};
+
+// Get Edit Product Form
+const getEditProduct = async (req, res) => {
+  const product = await Product.find({ _id: req.params.productID });
+  res.render("edit-product", {
     product,
   });
 };
 
+// Edit Product
 const editProduct = async (req, res) => {
   const product = await Product.findOneAndUpdate(
     { _id: req.params.productID },
@@ -26,20 +36,20 @@ const editProduct = async (req, res) => {
     }
   );
 
-  res.status(202).json({
-    product,
-  });
+  res.redirect("/admin");
 };
 
+// Delete Product
 const deleteProduct = async (req, res) => {
   const product = await Product.findOneAndDelete({ _id: req.params.productID });
-  res.status(204).send();
+  res.redirect("/admin");
 };
 
 module.exports = {
   getAllProducts,
   getAddProduct,
   createProduct,
+  getEditProduct,
   editProduct,
   deleteProduct,
 };
--- routes/admin.js
+++ routes/admin.js
@@ -3,13 +3,15 @@ const {
   getAllProducts,
   getAddProduct,
   createProduct,
+  getEditProduct,
   editProduct,
   deleteProduct,
 } = require("../controllers/adminController");
 const router = express.Router();
 
-router.route("/").get(getAllProducts).post(createProduct);
-router.route("/add-product").get(getAddProduct);
-router.route("/:productID").patch(editProduct).delete(deleteProduct);
+router.route("/").get(getAllProducts);
+router.route("/add-product").get(getAddProduct).post(createProduct);
+router.route("/edit-product/:productID").get(getEditProduct).post(editProduct);
+router.route("/delete-product/:productID").post(deleteProduct);
 
 module.exports = router;
